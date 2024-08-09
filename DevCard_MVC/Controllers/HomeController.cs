38707507b8fbf2aaa913e42d0e81bcbc0c4cee23
ali heydari <alirezaheydari.site@gmail.com>
2024-08-09 15:00:41 +0330

    create index and contact page and controller and fix links

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,29 +1,18 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
