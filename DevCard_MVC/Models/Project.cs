68eec83047b0d354e7f86e8797732cf74504b5cf
ali heydari <alirezaheydari.site@gmail.com>
2024-08-09 18:45:15 +0330

    Move Articles and Projects to ViewComponent and Move Footer and Header Layout WebSite To PartialView

--- DevCard_MVC/Models/Project.cs
+++ DevCard_MVC/Models/Project.cs
@@ -3,8 +3,18 @@
     public class Project
     {
         public int Id { get; set; }
-        public string NameTitle { get; set; }
+        public string Name { get; set; }
         public string Descrption { get; set; }
         public string Client { get; set; }
+        public string Image { get; set; }
+
+        public Project(int id, string nameTitle, string descrption, string client, string image)
+        {
+            Id = id;
+            Name = nameTitle;
+            Descrption = descrption;
+            Client = client;
+            Image = image;
+        }
     }
 }
---
+++ DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1,"مقاله اول من", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است",
+                "blog-post-thumb-card-1.jpg"),
+                new Article(2,"مقاله دوم من", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است",
+                "blog-post-thumb-card-2.jpg"),
+                new Article(3,"مقاله سوم من", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است",
+                "blog-post-thumb-card-4.jpg"),
+                new Article(4,"مقاله چهارم من", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است",
+                "blog-post-thumb-card-3.jpg"),
+                new Article(5,"مقاله پنجم من", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است",
+                "blog-post-thumb-card-5.jpg"),
+                 new Article(6,"مقاله ششم من", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است",
+                "blog-post-thumb-card-6.jpg"),
+            };
+            return View("_LatestArticles", articles.Take(3));
+        }
+    }
+}
