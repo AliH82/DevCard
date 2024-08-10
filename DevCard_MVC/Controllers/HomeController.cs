8f75a80ba588abadf68f7acd424bd543ba077e53
ali heydari <alirezaheydari.site@gmail.com>
2024-08-10 23:59:59 +0330

    create post method contact

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Service> _services = new List<Service>()
+        {
+            new Service(1,"طلایی"),
+            new Service(2,"نقره ای"),
+            new Service(3,"برنزه ای"),
+            new Service(4,"الماسی")
+        };
 
         public IActionResult Index()
         {
@@ -14,7 +23,28 @@ namespace DevCard_MVC.Controllers
 
         public IActionResult Contact()
         {
-            return View();
+            Contact model = new Contact()
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.error = "اطلاعات وارد شده صحیح نمی باشد";
+                model.Services = new SelectList(_services, "Id", "Name");
+                return View(model);
+            }
+            ViewBag.success = "پیغام شما با موفقیت ارسال شد";
+            ModelState.Clear();
+            model = new Contact();
+            model.Services = new SelectList(_services, "Id", "Name");
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DevCard_MVC/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevCard_MVC.Models
+{
+    public class Contact
+    {
+        //public int Id { get; set; }
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [MinLength(3,ErrorMessage = "طول نام باید بیشتر از 3 کاراکتر باشد")]
+        [MaxLength(100,ErrorMessage = "طول نام باید کمتر از 100 کاراکتر باشد")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده صحیح نیست")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        public string Message { get; set; }
+        //[Required(ErrorMessage = "این فیلد اجباری است")]
+        public SelectList Services { get; set; }
+    }
+}
